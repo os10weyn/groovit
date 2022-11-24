8e8d95f2f577d8b573f8f3bad08f1a5c58253f3c
Austin Wayne M. De Guzman <awmdeguzman.student@ua.edu.ph>
2022-11-24 09:52:10 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import './App.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './css/loginform.css';
+import './css/signupform.css';
+
+import './css/homepage.css';
+// import './css/carousel.css';
+
+import LoginForm from './js/loginform';
+import SignUpForm from './js/signupform';
+import HomePage from './js/homepage';
+import MPlayer from './js/playercomponents/index';
+function App() {
+  return (
+    <Router>
+    <div className="page">
+      <Switch>
+        <Route exact path='/'>
+          <LoginForm />
+        </Route>
+        <Route path='/signup'>
+          <SignUpForm/>
+        </Route>
+        <Route exact path='/home'>
+          <HomePage/>
+        </Route>
+        <Route exact path='/player'>
+          <MPlayer/>
+        </Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/js/home/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHome, faSearch, faMusic} from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+
+import { useState } from 'react';
+
+
+function Navigation() {
+    const  [active, setActive] = useState(0);
+  return (
+    <div className="nav-cont">
+        <nav className="navbar">
+            <a><FontAwesomeIcon icon={faHome} className="home-btn"  /></a>
+            <FontAwesomeIcon icon={faSearch} className="search-btn" />
+           <Link to ="/player"><FontAwesomeIcon icon={faMusic} className="music-btn" /> </Link>
+        </nav>
+    </div>
+  )
+
+
+}
+
+export default Navigation
